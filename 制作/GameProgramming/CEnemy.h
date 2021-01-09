cfa8eddd01c5538005109bd8c384e60b3231aabb
fujiwara <19010516@anabuki-net.ne.jp>
2021-01-09 15:45:17 +0900

    a

--- 制作/GameProgramming/CEnemy.h
+++ 制作/GameProgramming/CEnemy.h
@@ -1,19 +1,24 @@
-#ifndef CENEMY2_H
-#define CENEMY2_H
+#ifndef CENEMY_H
+#define CENEMY_H
 #include"CCharacter.h"
 #include"CCollider.h"
-#include"CPlayer.h"
+#include "CVector.h"
+#include"CSceneGame.h"
 
-
-class CEnemy2 : public CCharacter{
+class CEnemy : public CCharacter{
 public:
 	//デフォルトコンストラクタ(引数のないコンストラクタ）
-	CEnemy2(CModel*model, CVector position, CVector rotation, CVector scale);
+	CEnemy(CModel*model, CVector position, CVector rotation, CVector scale);
 	CCollider mColBody;
+
 	//衝突判定
 	void Collision(CCollider*m, CCollider*y);
+	void Update();
+	int state;
+	int frame;
 
 };
+
 
 
 #endif
---
+++ 制作/GameProgramming/CEnemy2.cpp
@@ -0,0 +1,42 @@
+#include"CEnemy2.h"
+
+
+//課題7
+CEnemy2::CEnemy2(CModel*model, CVector position, CVector rotation, CVector scale)
+:mColBody(this, CVector(0.0f, 0.0f, 0.0f), CVector(0.0f, 0.0f, 0.0f),
+CVector(1.0f, 1.0f, 1.0f), 1.0f)
+{
+	//モデル、位置、回転、拡縮を設定する
+	mpModel = model; //モデルの設定
+	mPosition = position; //位置の設定
+	mRotation = rotation; //回転の設定
+	mScale = scale; //拡縮の設定
+
+	mColBody.mTag = CCollider::EBOMB;
+}
+
+//課題8
+void CEnemy2::Collision(CCollider*m, CCollider*y){
+
+	//相手がプレイヤー
+	if (y->mTag == CCollider::EBODY){
+		//衝突
+		if (CCollider::Collision(m, y)){
+			//衝突しているときは無効にする
+			mEnabled = false;
+		}
+	}
+
+
+
+	//引き寄せ
+	if (y->mTag == CCollider::ESEARCH){
+		if (CCollider::Collision(m, y)){
+			//プレイヤーの方向
+			CVector dir = y->mpParent->mPosition - mPosition;
+			//正規化（長さを1にする）Normalize()
+			mPosition = mPosition + dir.Normalize()*0.5;
+		}
+	}
+}
+
